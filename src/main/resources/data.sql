a318b726d07f6b8cbc89e3d2c762feda28bb9b41
Ruben Martinez Barragan <rumartin@LES003045.corp.capgemini.com>
2023-06-07 13:52:05 +0200

    Correcion fecha campo 'date_return' segundo registro tabla 'loan'.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -28,7 +28,7 @@ INSERT INTO client(name) VALUES ('Client 5');
 
 --Tabla loan (prestamo):
 INSERT INTO loan(game_id, client_id, date_loan, date_return) VALUES (1, 1, '2020-01-01', '2020-01-06');
-INSERT INTO loan(game_id, client_id, date_loan, date_return) VALUES (2, 1, '2020-01-02', '2020-02-14');
+INSERT INTO loan(game_id, client_id, date_loan, date_return) VALUES (2, 1, '2020-01-02', '2020-01-14');
 INSERT INTO loan(game_id, client_id, date_loan, date_return) VALUES (1, 1, '2020-01-07', '2020-01-14');
 INSERT INTO loan(game_id, client_id, date_loan, date_return) VALUES (3, 2, '2020-01-01', '2020-01-09');
 INSERT INTO loan(game_id, client_id, date_loan, date_return) VALUES (4, 3, '2020-01-01', '2020-01-10');
